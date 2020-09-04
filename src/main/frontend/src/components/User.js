580b82d905fc8e5ad76c9881ebcf4a1579085d65
grzes <Antrykot123>
2020-09-05 00:31:59 +0200

    Presentation of trainers in 2 columns, links to id of every trainer in every trainer jumbotron

--- src/main/frontend/src/components/User.js
+++ src/main/frontend/src/components/User.js
@@ -1,18 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
+import {Link, useLocation, useParams} from "react-router-dom"
+import {Form, InputGroup, FormControl, Button, Container, Jumbotron, Alert} from 'react-bootstrap'
 import Header from "./Header";
-// import { Form, InputGroup, FormControl, Button } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const Styles = styled.div`
+    .jumbo {
+        cursor: pointer;
+        text-decoration: none;
+        color: inherit;
+    }
+`;
 
 function User(props) {
-    const { name, surname, password, createDate, lastLogin } = props
+    const {userId, name, surname, password, createDate, lastLogin} = props
+    let location = useLocation()
+    const {id} = useParams()
+    const userLink = "/main/" + userId
 
     return (
-        <div>
-            <div>
-                <Header title={name}/>
-                <p>{surname}</p>
-                <p>{createDate}</p>
-            </div>
-        </div>
+        <Styles>
+            <Link to={userLink} className="jumbo">
+                <Jumbotron>
+                    <Container>
+                        <Header title={name}/>
+                        <p>{surname}</p>
+                        <p>{createDate}</p>
+                    </Container>
+                </Jumbotron>
+            </Link>
+        </Styles>
     )
 }
+
 export default User

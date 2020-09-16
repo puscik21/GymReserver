b74c72baf846edee939ebf20c2a6088e632ec89b
grzes <Antrykot123>
2020-09-16 23:13:11 +0200

    test commit for huroku

--- src/main/java/com/zti/gymreserver/reservation/DateConverter.java
+++ src/main/java/com/zti/gymreserver/reservation/DateConverter.java
@@ -7,13 +7,27 @@ public class DateConverter {
     public static String hoursIdToString(int hoursId) {
         String result = "";
         switch (hoursId) {
-            case 0 -> result = "08:00 - 10:00";
-            case 1 -> result = "10:00 - 12:00";
-            case 2 -> result = "12:00 - 14:00";
-            case 3 -> result = "14:00 - 16:00";
-            case 4 -> result = "16:00 - 18:00";
-            case 5 -> result = "18:00 - 20:00";
-            case 6 -> result = "20:00 - 22:00";
+            case 0:
+                result = "08:00 - 10:00";
+                break;
+            case 1:
+                result = "10:00 - 12:00";
+                break;
+            case 2:
+                result = "12:00 - 14:00";
+                break;
+            case 3:
+                result = "14:00 - 16:00";
+                break;
+            case 4:
+                result = "16:00 - 18:00";
+                break;
+            case 5:
+                result = "18:00 - 20:00";
+                break;
+            case 6:
+                result = "20:00 - 22:00";
+                break;
         }
         return result;
     }
